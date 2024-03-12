36d56a4eee5b6e11de610991c8ce29f362b212ca
Fredy <fredy6786@gmail.com>
2024-03-12 08:04:12 +0100

    update de rolescreate

--- src/commands/administration/rolescreate.ts
+++ src/commands/administration/rolescreate.ts
@@ -21,12 +21,19 @@ export class RoleCreateCommand extends Command {
         const embed = new EmbedBuilder()
             .setTitle('Choix des roles')
             .setDescription(`
-                Maintenant que vous avez accepté le <#963041730066804786> choisissez le/les roles qui vous correspondent
+                Maintenant que vous avez accepté le <#963041730066804786> choisissez le ou les roles qui vous correspondent
+
+                👨‍🦰 Si tu est un homme
+                👩‍🦰 Si tu est une femme
+                🚚 Si tu est un chauffeur
+                🚜 Si tu est un agriculteur
             `)
             .setTimestamp()
             .setFooter({ text: interaction.user.username, iconURL: interaction.user.displayAvatarURL() })
 
             interaction.channel?.send({ embeds: [embed] });
+
+            interaction.reply({ content: 'Les roles réactions ont bien été activé', ephemeral: true });
 
   }
 }
