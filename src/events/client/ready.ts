907646fdd0eb522ed7e853b3a05c9fc03fef9f6d
Fredy <fredy6786@gmail.com>
2024-03-13 17:44:29 +0100

    update du règlement

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -59,16 +59,11 @@ export class ReadyEvent extends Event {
       var corsOptions = {
         // @ts-expect-error
         origin: function (origin, callback) {
-          if(origin == undefined) {
-            callback(new Error('Not allowed by CORS'))
-          }
-          else {
             if (origin !== undefined || whitelist.indexOf(origin) !== -1) {
               callback(null, true)
             } else {
               callback(new Error('Not allowed by CORS'))
             }
-          }
           
         }
       }
