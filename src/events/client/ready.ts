b7a27a81d76bf39029834f7ff073746f5932fb78
Fredy <fredy6786@gmail.com>
2024-03-13 16:12:56 +0100

    update du règlement

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -55,7 +55,7 @@ export class ReadyEvent extends Event {
     const app = express();
     const port = 3000;
     var corsOptions = {
-      origin: '127.0.0.1',
+      origin: 'localhost',
       optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
     }
 
