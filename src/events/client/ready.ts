74acb58a11650cd888568a40a156ed95b7fa65b9
Fredy <fredy6786@gmail.com>
2024-03-17 19:52:47 +0100

    teste

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -60,7 +60,7 @@ export class ReadyEvent extends Event {
     const allowLocalhostOnly = (req:any) => {
       const remoteAdress = req.ip;
 
-      if (remoteAdress != '127.0.0.1' || remoteAdress != '::ffff:127.0.0.1' || remoteAdress != '::1') {
+      if (remoteAdress !== '127.0.0.1' || remoteAdress !== '::ffff:127.0.0.1' || remoteAdress !== '::1') {
         req.status(403).send('Forbidden');
       }
     };
