dbfa2ce11da4f73d8f9a14f25f74472191956f26
Fredy <fredy6786@gmail.com>
2024-03-17 18:00:33 +0100

    debug cors

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -60,7 +60,7 @@ export class ReadyEvent extends Event {
     const allowLocalhostOnly = (req, res, next) => {
       const remoteAdress = req.ip;
 
-      if (remoteAdress === '127.0.0.1' || remoteAdress === '::ffff:127.0.0.1' || remoteAdress === '::1') {
+      if (remoteAdress === '127.0.0.1') {
         next();
       }
       else {
