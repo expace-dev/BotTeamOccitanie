69e50fc85b7f1bda36c7bc60e5a8cb9ade7ba92c
Fredy <fredy6786@gmail.com>
2024-03-17 13:11:44 +0100

    evenement

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -218,7 +218,7 @@ export class ReadyEvent extends Event {
         image: {
           url: req.query.image,
         },
-        description: 'Rejoignez fredy34560 sur TruckerMp le <F:1618953630:d>',
+        description: `Rejoignez fredy34560 sur TruckerMp le <F:1618953630:d>`,
         footer: {
           text: req.query.username,
           icon_url: req.query.avatar,
