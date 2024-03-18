efb0987acc3a10744a3fce60bc082d736d4ffe1b
Fredy <fredy6786@gmail.com>
2024-03-18 08:43:20 +0100

    debug cors

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -74,16 +74,11 @@ export class ReadyEvent extends Event {
    //var whitelist = ['http://127.0.0.1']
     
    
-   var allowedOrigins = ['http://yourapp.com'];
+   //ar allowedOrigins = ['http://yourapp.com'];
   
   app.use(cors({
   origin: function(origin, callback){    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }    return callback(null, true);
+    console.log(origin);
   }
 }));
 
